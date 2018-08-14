940d1742bf4a7c10bc2f12926ce942d3d11fa342
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 02:55:03 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,4 +1,6 @@
 def meal_choice(name = "meat")
-  
+  puts "meal_choice"
+end
+meal_choice("sushi")
    # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
